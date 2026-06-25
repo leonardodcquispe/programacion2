33a3c242b9bde1278dfc1f89c3056235adc18a93
quispesiño <ljamierdelacruz@gmail.com>
2026-06-25 08:20:53 -0300

    Add files via upload

---
+++ e01.py
@@ -0,0 +1,14 @@
+usuario=""
+añodenacimiento=0
+añoactual=0
+suma=0
+
+print("hola usuario,como te llamas?")
+usuario=int()
+print("bueno,ahora me podes decir tu año de nacimiento")
+añodenacimiento=int(input())
+print("ahora decime que año es actual")
+añoactual=int(input())
+
+suma=añoactual-añodenacimiento
+print("tenes",suma)
