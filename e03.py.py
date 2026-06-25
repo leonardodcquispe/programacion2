868a568fa55dc58afb770db022721e9eaa22fdc6
quispesiño <ljamierdelacruz@gmail.com>
2026-06-25 08:23:24 -0300

    Add files via upload

---
+++ e03.py.py
@@ -0,0 +1,7 @@
+# Diseñar un programa que el usuario ingrese un número y desde que sistema hacia que sistema de númeración.
+#Además mostrar el carácter ASCII que corresponde al número
+
+n=int(input("Ingresa un número: "))
+print("Binario: ", bin(n))
+print("Hexadecimal: ", hex(n))
+print("ASCII: ", ascii(n))
